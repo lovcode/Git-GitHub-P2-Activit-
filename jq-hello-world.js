8eddf8dd6309a97a79267400bda9ca75686854d7
lovcode <muriel_guerin@yahoo.fr>
2017-05-09 15:14:06 +0200

    Affichage Ligne de texte supplémentaire

--- jq-hello-world.js
+++ jq-hello-world.js
@@ -1,3 +1,4 @@
 $(function() {
   $('#texteJQ').html('Hello world. Ce texte est affiché par jQuery.');
+  $('#texteJQ').after(' Ceci est un changement supplémentaire pour l\'activité GitHub !');
 });
